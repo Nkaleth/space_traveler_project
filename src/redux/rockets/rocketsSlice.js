11a0ee119bb8ae272ebaa47a4343052f32e1935a
Nkaleth <nseguralu@gmail.com>
2023-03-07 12:17:35 -0500

    ADD function to fetch rocket data

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://api.spacexdata.com/v3/rockets';
 
 const initialState = {
   rocketItems: [],
   isReserve: false,
 };
+
+export const getRocketItems = createAsyncThunk('rockets/getRocketItems', async () => {
+  try {
+    const resp = await axios.get(url);
+    return resp.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const rocketsSlice = createSlice({
   name: 'rockets',
