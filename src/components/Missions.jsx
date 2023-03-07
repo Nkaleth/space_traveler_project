c56e23276ac5daa34e23ea40eabb406ffd153221
DarkxDev <bryanfernandohurtado@gmail.com>
2023-03-07 11:50:33 -0500

    Fetch missions on and dispatch selected data

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,31 @@
-const MissionsPage = () => (
-  <div>
-    <h1>Missions Page</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missionsSlice';
+
+const MissionsPage = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (missions.length > 0) {
+      const selectedMissions = missions.map((mission) => ({
+        mission_id: mission.mission_id,
+        mission_name: mission.mission_name,
+        description: mission.description,
+      }));
+      dispatch({ type: 'missions/selectedMissions', payload: selectedMissions });
+    }
+  }, [missions, dispatch]);
+
+  return (
+    <div>
+      <h1>Missions Page</h1>
+    </div>
+  );
+};
 
 export default MissionsPage;
