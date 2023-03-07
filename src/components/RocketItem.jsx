e3185032e91e10fea63b0e3a4478c0e1af4f4deb
Nkaleth <nseguralu@gmail.com>
2023-03-07 17:27:24 -0500

    Add rockets rendering with only plain html

--- src/components/RocketItem.jsx
+++ src/components/RocketItem.jsx
@@ -2,22 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const RocketItem = ({
-  id, name, type, Images,
+  id, name, type, Image,
 }) => (
   <div className="rocketItemCont">
-    <h2>ROCKETs HERE!</h2>
     <h2>{name }</h2>
     <p>{id}</p>
     <p>
       {type}
     </p>
-    <img src={Images[0]} alt="" />
+    <img src={Image[0]} alt="" />
   </div>
 );
 
 RocketItem.propTypes = { id: PropTypes.string.isRequired };
 RocketItem.propTypes = { name: PropTypes.string.isRequired };
 RocketItem.propTypes = { type: PropTypes.string.isRequired };
-RocketItem.propTypes = { Images: PropTypes.string.isRequired };
+RocketItem.propTypes = { Image: PropTypes.string.isRequired };
 
 export default RocketItem;
