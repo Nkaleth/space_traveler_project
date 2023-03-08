7bc616265e698f92c8086730ac734d2eb810f3c2
Nkaleth <nseguralu@gmail.com>
2023-03-08 12:06:36 -0500

    FEATURE: Add reserve rocket and show it on My Profile page

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,7 +1,21 @@
-const MyProfilePage = () => (
-  <div>
-    <h1>My Profile Page</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const MyProfilePage = () => {
+  const { rocketItems } = useSelector((store) => store.rockets);
+  const reserveRockets = rocketItems.filter((rocket) => rocket.reserved === true);
+  return (
+    <div>
+      <hr />
+      <section className="myRockets">
+        <h2>My Rockets</h2>
+        {reserveRockets.map((rockets) => (
+          <li key={rockets.id}>
+            {rockets.name }
+          </li>
+        ))}
+      </section>
+    </div>
+  );
+};
 
 export default MyProfilePage;
