64a0a0f074c828e234d2febfd173fab2883d3a45
DarkxDev <bryanfernandohurtado@gmail.com>
2023-03-08 15:11:19 -0500

    Added missions to profile page

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,7 +1,28 @@
-const MyProfilePage = () => (
-  <div>
-    <h1>My Profile Page</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const MyProfilePage = () => {
+  const missions = useSelector((state) => state.missions.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+
+  return (
+    <div className="profile-section">
+      <hr className="missions-divider" />
+      <section className="profile-missions">
+        <h1>My Missions</h1>
+        <ul>
+          {joinedMissions.length > 0 ? (
+            joinedMissions.map((mission) => (
+              <li key={mission.mission_id} className="profile-missions-item">
+                {mission.mission_name}
+              </li>
+            ))
+          ) : (
+            <li>No joined missions.</li>
+          )}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default MyProfilePage;
